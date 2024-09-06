dd607f883db5c14d486dad8ef9c127684d59de67
윤태원 <ytw0728@gmail.com>
2024-09-07 06:01:15 +0900

    pixi init이 top-level-await에서 오동작하는 이슈 대응

--- apps/client/src/index.ts
+++ apps/client/src/index.ts
@@ -17,12 +17,6 @@ import { ScoreBoard } from 'ui/ScoreBoard'
 import { MAX_ROUND, PLAYER_COUNT, validate } from 'utils/validater'
 
 const app = new Application()
-await app.init({
-  resolution: Math.max(window.devicePixelRatio, 2),
-  backgroundColor: 0xffffff,
-  resizeTo: window,
-})
-document.getElementById('app')?.appendChild(app.canvas)
 
 export const sheet = new Spritesheet(await Assets.load('d6Red.png'), (await Assets.load('d6Red.json')).data)
 await sheet.parse()
@@ -43,174 +37,185 @@ export const DiceList = new List<AnimatedSprite | FancyButton>({
   elementsMargin: 16,
   padding: 10,
 })
-DiceList.on('childAdded', () => {
-  DiceList.position.set((app.screen.width - DiceList.width) / 2, 800)
-})
-DiceList.on('childRemoved', () => {
-  DiceList.position.set((app.screen.width - DiceList.width) / 2, 800)
-})
-
 export const UserList = new List<ScoreBoard>({
   children: [],
   type: 'horizontal',
   elementsMargin: 16,
   padding: 10,
 })
-UserList.on('childAdded', () => {
-  UserList.position.set((app.screen.width - UserList.width) / 2, 30)
-})
-UserList.on('childRemoved', () => {
-  UserList.position.set((app.screen.width - UserList.width) / 2, 30)
-})
-
 export const ActionList = new List<GraphicButton>({
   children: [],
   type: 'horizontal',
   elementsMargin: 16,
   padding: 10,
 })
-ActionList.on('childAdded', () => {
-  ActionList.position.set((app.screen.width - ActionList.width) / 2, app.screen.height - ActionList.height - 60)
-})
-ActionList.on('childRemoved', () => {
-  ActionList.position.set((app.screen.width - ActionList.width) / 2, app.screen.height - ActionList.height - 60)
-})
 
-$Users.subscribe((users) => {
-  if (users.length === 0) {
-    $Game.reset()
-  }
-  if (users.length >= PLAYER_COUNT.min && users.length <= PLAYER_COUNT.max) {
-    $Game.ready()
-  }
-  users.map((user) => $Boards.register(user.info.id))
-})
+void app
+  .init({
+    resolution: Math.max(window.devicePixelRatio, 2),
+    backgroundColor: 0xffffff,
+    resizeTo: window,
+  })
+  .then(() => {
+    document.getElementById('app')?.appendChild(app.canvas)
+  })
+  .then(() => {
+    DiceList.on('childAdded', () => {
+      DiceList.position.set((app.screen.width - DiceList.width) / 2, 800)
+    })
+    DiceList.on('childRemoved', () => {
+      DiceList.position.set((app.screen.width - DiceList.width) / 2, 800)
+    })
 
-$Boards.subscribe((boards) => {
-  const users = $Users.get()
+    UserList.on('childAdded', () => {
+      UserList.position.set((app.screen.width - UserList.width) / 2, 30)
+    })
+    UserList.on('childRemoved', () => {
+      UserList.position.set((app.screen.width - UserList.width) / 2, 30)
+    })
+    ActionList.on('childAdded', () => {
+      ActionList.position.set((app.screen.width - ActionList.width) / 2, app.screen.height - ActionList.height - 60)
+    })
+    ActionList.on('childRemoved', () => {
+      ActionList.position.set((app.screen.width - ActionList.width) / 2, app.screen.height - ActionList.height - 60)
+    })
+    app.stage.addChild(DiceList)
+    app.stage.addChild(UserList)
+    app.stage.addChild(ActionList)
+  })
+  .then(() => {
+    $Users.subscribe((users) => {
+      if (users.length === 0) {
+        $Game.reset()
+      }
+      if (users.length >= PLAYER_COUNT.min && users.length <= PLAYER_COUNT.max) {
+        $Game.ready()
+      }
+      users.map((user) => $Boards.register(user.info.id))
+    })
 
-  const registeredUserID = UserList.children.map((child) => child.id)
+    $Boards.subscribe((boards) => {
+      const users = $Users.get()
 
-  for (const [id, board] of Object.entries(boards)) {
-    const target = users.find((user) => user.info.id === id)
-    if (!target) {
-      return
-    }
-    if (!registeredUserID.includes(id)) {
-      const scoreBoard = new ScoreBoard(target, board)
-      UserList.addChild(scoreBoard)
-      return
-    }
+      const registeredUserID = UserList.children.map((child) => child.id)
 
-    UserList.children.find((value) => value.id === id)?.setBoard(board)
-  }
-})
+      for (const [id, board] of Object.entries(boards)) {
+        const target = users.find((user) => user.info.id === id)
+        if (!target) {
+          return
+        }
+        if (!registeredUserID.includes(id)) {
+          const scoreBoard = new ScoreBoard(target, board)
+          UserList.addChild(scoreBoard)
+          return
+        }
 
-$Game.subscribe(async (game) => {
-  await allTasks()
-  switch (game.stage) {
-    case GameStage.WAITING:
-      ActionList.removeChildren()
-      ActionList.addChild(UserAddButton)
-      return
-    case GameStage.READY: {
-      ActionList.removeChildren()
-      ActionList.addChild(UserAddButton)
-      ActionList.addChild(StartButton)
-      break
-    }
-    case GameStage.PLAYING: {
-      ActionList.removeChild(StartButton)
-      ActionList.removeChild(UserAddButton)
-      ActionList.addChild(RollDiceButton)
-      ActionList.addChild(TurnEndButton)
+        UserList.children.find((value) => value.id === id)?.setBoard(board)
+      }
+    })
 
-      if (game.round >= MAX_ROUND) {
-        $Game.set({
-          ...game,
-          stage: GameStage.FINISH,
-          winner: validate({
-            boards: $Boards.get(),
-            currentRound: game.round - 1,
-          }).userRank[0].userID,
-        })
-      } else {
-        for (const child of UserList.children) {
-          child.setTurn(game.turn ?? '')
+    $Game.subscribe(async (game) => {
+      await allTasks()
+      switch (game.stage) {
+        case GameStage.WAITING:
+          ActionList.removeChildren()
+          ActionList.addChild(UserAddButton)
+          return
+        case GameStage.READY: {
+          ActionList.removeChildren()
+          ActionList.addChild(UserAddButton)
+          ActionList.addChild(StartButton)
+          break
+        }
+        case GameStage.PLAYING: {
+          ActionList.removeChild(StartButton)
+          ActionList.removeChild(UserAddButton)
+          ActionList.addChild(RollDiceButton)
+          ActionList.addChild(TurnEndButton)
+
+          if (game.round >= MAX_ROUND) {
+            $Game.set({
+              ...game,
+              stage: GameStage.FINISH,
+              winner: validate({
+                boards: $Boards.get(),
+                currentRound: game.round - 1,
+              }).userRank[0].userID,
+            })
+          } else {
+            for (const child of UserList.children) {
+              child.setTurn(game.turn ?? '')
+            }
+          }
+          return
+        }
+
+        case GameStage.FINISH: {
+          const user = $Users.get().find((u) => u.info.id === game.winner)
+          const nickname = user?.info.nickname ?? ''
+          const score = Object.values($Boards.getAll(user?.info.id ?? '')?.records ?? {}).reduce(
+            (prev, curr) => prev + curr.score,
+            0,
+          )
+
+          const endButton = new GraphicButton(
+            new FancyText({
+              text: `${nickname} 승리! (${score} 점)`,
+              style: { fontSize: 32, fontWeight: 'bold' },
+            }),
+          )
+          endButton.position.set(50, 0)
+          const onClick = () => {
+            $Game.regame()
+            $Boards.erase()
+            $Dice.reset()
+          }
+          endButton.onPress.connect(onClick)
+          endButton.on('touchstart', onClick)
+
+          ActionList.removeChildren()
+          ActionList.addChild(endButton)
+          return
         }
       }
-      return
-    }
+    })
 
-    case GameStage.FINISH: {
-      const user = $Users.get().find((u) => u.info.id === game.winner)
-      const nickname = user?.info.nickname ?? ''
-      const score = Object.values($Boards.getAll(user?.info.id ?? '')?.records ?? {}).reduce(
-        (prev, curr) => prev + curr.score,
-        0,
-      )
+    $Dice.subscribe(({ dices, step }) => {
+      const currentGame = $Game.get()
+      if (currentGame.stage !== GameStage.PLAYING) return
+      if (currentGame.turn === null) return
 
-      const endButton = new GraphicButton(
-        new FancyText({
-          text: `${nickname} 승리! (${score} 점)`,
-          style: { fontSize: 32, fontWeight: 'bold' },
-        }),
-      )
-      endButton.position.set(50, 0)
-      const onClick = () => {
-        $Game.regame()
-        $Boards.erase()
-        $Dice.reset()
+      if (0 === step) {
+        for (const child of UserList.children) {
+          child.setDice(null)
+        }
+        DiceList.removeChildren()
       }
-      endButton.onPress.connect(onClick)
-      endButton.on('touchstart', onClick)
-
-      ActionList.removeChildren()
-      ActionList.addChild(endButton)
-      return
-    }
-  }
-})
-
-$Dice.subscribe(({ dices, step }) => {
-  const currentGame = $Game.get()
-  if (currentGame.stage !== GameStage.PLAYING) return
-  if (currentGame.turn === null) return
-
-  if (0 === step) {
-    for (const child of UserList.children) {
-      child.setDice(null)
-    }
-    DiceList.removeChildren()
-  }
-  if (0 < step && step <= MAX_DICE_STEP) {
-    for (const child of UserList.children) {
-      if (child.id === currentGame.turn) {
-        console.log(
-          dices.map(({ value }) => value),
-          step,
-        )
-        child.setDice({ dices, step })
-      } else {
-        child.setDice(null)
+      if (0 < step && step <= MAX_DICE_STEP) {
+        for (const child of UserList.children) {
+          if (child.id === currentGame.turn) {
+            console.log(
+              dices.map(({ value }) => value),
+              step,
+            )
+            child.setDice({ dices, step })
+          } else {
+            child.setDice(null)
+          }
+        }
       }
-    }
-  }
-})
+    })
 
-$TemporaryScore.subscribe(async (temporary) => {
-  await allTasks()
+    $TemporaryScore.subscribe(async (temporary) => {
+      await allTasks()
 
-  const game = $Game.get()
-  if (temporary) {
-    for (const child of UserList.children) {
-      if (game.turn === child.id) {
-        child.render()
+      const game = $Game.get()
+      if (temporary) {
+        for (const child of UserList.children) {
+          if (game.turn === child.id) {
+            child.render()
+          }
+        }
       }
-    }
-  }
-})
-
-app.stage.addChild(DiceList)
-app.stage.addChild(UserList)
-app.stage.addChild(ActionList)
+    })
+  })
